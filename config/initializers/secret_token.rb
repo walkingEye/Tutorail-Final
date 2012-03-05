4b608ea6cfa7e90810c96f30525fcf64130cb0a7
Paul Barnes <paul.barnes@avayagov.com>
2012-03-05 13:33:52 -0500

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalApp::Application.config.secret_token = 'bbafdd75ec9d0408430f4fea9c395d7317e0e5a202e7a1ff4014819e3c80fadbab02892b525532870be433888ba5328474aeeff50780969b9c560fa95024bff2'
