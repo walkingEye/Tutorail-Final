4739c4a120bd9bdc92dca0fd92e84497a78254f6
Paul Barnes <paul.barnes@avayagov.com>
2012-03-15 12:55:25 -0400

    Built relationships for following/unfollowing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,26 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean
 #
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
   before_save :create_remember_token
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -26,6 +35,18 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
   
+  def following?(other_user)
+    self.relationships.find_by_followed_id(other_user.id)
+  end
+  
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    self.relationships.find_by_followed_id(other_user.id).destroy
+  end
+ 
   private
   
     def create_remember_token
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,24 +15,35 @@ describe "Static pages" do
     let(:page_title) { 'Home' }
     
     it_should_behave_like "all static pages"
-    end
 
-  describe "for signed-in users" do
-    let(:user) { FactoryGirl.create(:user) }
-    before do
-      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-      FactoryGirl.create(:micropost, user: user, content: "Foobar")
-      sign_in user
-      visit root_path
-    end
-
-    it "should render the user's feed" do
-      user.feed.each do |item|
-        page.should have_selector("tr##{item.id}", text: item.content)
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Foobar")
+        sign_in user
+        visit root_path
       end
-    end
-  end 
-
+  
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("tr##{item.id}", text: item.content)
+        end
+      end
+      
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+        
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 follower",  href: followers_user_path(user)) }
+      end
+    end 
+  end
+  
   describe "Help page" do
     before { visit help_path }
     let(:heading) { 'Help' }
